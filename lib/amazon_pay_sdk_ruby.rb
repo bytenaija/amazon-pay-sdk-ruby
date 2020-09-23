4f112cccc186c9e026401c65e1c9a0f4e97e792c
Everistus Olumese <everistusolumese@gmail.com>
2020-09-23 10:08:51 +0100

    Amazom Pay AI SDK (Ruby) 2.0.1

--- lib/amazon_pay_sdk_ruby.rb
+++ lib/amazon_pay_sdk_ruby.rb
@@ -2,3 +2,11 @@ require 'amazon_pay/version'
 require 'amazon_pay/amazon_pay_client'
 require 'amazon_pay/amazon_web_store_client'
 require 'amazon_pay/amazon_in_store_client'
+
+# Top-level module for the gem
+module AmazonPaySdkRuby
+  # Your code goes here...
+end
+
+# General Error class for the gem
+class AmazonPaySdkRuby::Error < StandardError; end
--- spec/config.rb
+++ spec/config.rb
@@ -2,7 +2,7 @@ module Config
   def self.get_config
     {
       'public_key_id': '', # Enter your Public Key ID
-      'private_key': File.read(File.expand_path('tst/private.pem', __dir__)), # Path to your private key file
+      'private_key': File.read(File.expand_path('tst/private.pem', __dir__)), # Path to your private key file or replace with your private key string
       'region': 'us',
       'sandbox': true,
       'currency_code': 'USD',
